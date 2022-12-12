89ceaedd11019695cb583fed05793a85cf929844
Aurelien Girona <agirona@z4r7p5.42lyon.fr>
2022-12-12 19:42:54 +0100

    good BST implementation, created insert and erase function

--- map/avl.hpp
+++ map/avl.hpp
@@ -6,7 +6,7 @@
 /*   By: agirona <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 17:40:51 by agirona           #+#    #+#             */
-/*   Updated: 2022/12/08 20:09:50 by agirona          ###   ########lyon.fr   */
+/*   Updated: 2022/12/12 19:42:10 by agirona          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,41 +29,50 @@ namespace ft
 			elem(key_type key);
 			~elem();
 
-			void 		set_parent(const elem<T, V> &parent);
-			void 		set_left(const elem<T, V> &left);
-			void 		set_right(const elem<T, V> &right);
-			elem<T, V>	*get_parent() const;
-			elem<T, V>	*get_left() const;
-			elem<T, V>	*get_right() const;
+			void 		set_parent(elem *parent);
+			void 		set_left(elem *left);
+			void 		set_right(elem *right);
+			void 		set_key(const T &key);
+			elem		*get_parent() const;
+			elem		*get_left() const;
+			elem		*get_right() const;
+			key_type	get_key() const;
 
 		private :
 			key_type	_key;
 			value_type	_value;
-			elem<T, V>	*_parent;
-			elem<T, V>	*_left;
-			elem<T, V>	*_right;
+			elem		*_parent;
+			elem		*_left;
+			elem		*_right;
 	};
 
 
-	template <class T, class Alloc = std::allocator<T> >
+	template <class T, class Alloc = std::allocator<elem<T, T> >, class compare = std::less<T> >
 	class	tree
 	{
 		public :
 			typedef	elem<T, T>							node;
 			typedef	Alloc								allocator_type;
-			typedef	random_access_iterator<node>		iterator; //tmp, replace by bidirectionnal
+			typedef compare								cmp_type;
 
 			tree();
 			~tree();
 
 			void 		insert(const T &val);
+			void 		erase(const T &val);
+			node		*get_origin();
 
 		private :
-			node			_origin;
+			node			*_origin;
 			size_t			_size;
 			allocator_type	_alloc;
+			cmp_type		cmp;
 
-			pointer		new_node();
+			node		*new_node(const T &val);
+			elem<T, T>	*oblitarate(elem<T, T> &current, const int &direction);
+			void		single_oblitarate(elem<T, T> &current);
+			void		complex_oblitarate(elem<T, T> &current);
+			void		clear_node(elem<T, T> *to_clear);
 
 	};
 };
